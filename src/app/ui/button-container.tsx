23cb3a96616f4b3629d3ffab61b59c3ae1d98489
Business.taphouse.games <andreasfreiburg85@gmail.com>
2024-03-23 14:31:22 -0600

    add chatbot logo and button link

--- src/app/ui/button-container.tsx
+++ src/app/ui/button-container.tsx
@@ -57,16 +57,21 @@ const ButtonContainer = () => {
         url="https://crookedblackheart.itch.io/"
         logo="/assets/logos/itch.png"
       />
-
       <Button
         text={"Dance Floor Shader Code"}
         url="https://godotshaders.com/shader/dance-floor-shader/"
         logo="/assets/logos/godot.webp"
       />
+
       <Button
         text={"Portfolio Source Code"}
         url="https://github.com/TaphouseGames/Andreas-Portfolio"
         logo="/assets/logos/github.png"
+      />
+      <Button
+        text={"Andreas Freiburg chatbot"}
+        url="https://andreas-freiburg-chatbot.vercel.app"
+        logo="/assets/logos/chatbot.jpg"
       />
     </div>
   );
